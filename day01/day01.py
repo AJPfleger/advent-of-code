6f57d046a29c71b70f7a63e5800b805b72c72f8b
AJPfleger <alexander.julian.pfleger@cern.ch>
2022-12-01 11:17:41 +0100

    add: day01

---
+++ day01/day01.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 07:56:39 2022
+
+@author: AJPfleger
+
+https://adventofcode.com/2022/day/1
+"""
+
+import numpy as np
+
+file = open('day1input.txt', 'r')
+Lines = file.readlines()
+
+calPerElf = [0]
+elfCounter = 0
+for i in range(len(Lines)):
+    if Lines[i] != '\n':
+        calItem = int(Lines[i])
+        calPerElf[-1] += calItem
+    else:
+        calPerElf.append(0)
+
+calPerElf.sort(reverse=True)
+print(f'Elf with the most calories carries {calPerElf[0]} calories.')
+
+topCals = 3
+calSum = 0
+for i in range(topCals):
+    calSum += calPerElf[i]
+
+print(f'The {topCals} elves with the most calories carry together {calSum} calories.')
+
