c7dc99c85475c1a56ad75fedb8fb45115debcdfa
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-02-10 00:11:00 +0100

    add: 2015/day05p2

--- 2015/day05/day05.sh
+++ 2015/day05/day05.sh
@@ -9,16 +9,18 @@ echo "--- Day 2: I Was Told There Would Be No Math ---"
 
 filename=input.txt
 
+
+# Part 1
+
 nicecount=0
 while read line; do
+  break
 
   # skip ab, cd, pq, or xy
   if [[ $line == *"ab"* ]] || [[ $line == *"cd"* ]] || [[ $line == *"pq"* ]] || [[ $line == *"xy"* ]]
   then
     continue
   fi
-
-  res="${line//[^a]}"
 
   ((vowels=${#${line//[^a]}}+${#${line//[^e]}}+${#${line//[^i]}}+${#${line//[^o]}}+${#${line//[^u]}}))
   if [[ $vowels -lt 3 ]]
@@ -40,3 +42,24 @@ while read line; do
 done < $filename
 
 echo "Part 1: ${nicecount} nice strings."
+
+
+# Part 2
+
+nicecount=0
+while read line; do
+
+  if ! echo "$line" | grep -Eq '([a-z])[a-z]\1'
+  then
+    continue
+  fi
+
+  if ! echo "$line" | grep -Eq '([a-z]{2,}).*\1'
+  then
+    continue
+  fi
+
+  ((nicecount+=1))
+done < $filename
+
+echo "Part 2: ${nicecount} nice strings."
