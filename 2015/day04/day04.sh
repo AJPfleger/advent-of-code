693e93416afb202b882f498ad37b7ea2ba338347
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-02-09 22:35:05 +0100

    add: 2015/day04p1

---
+++ 2015/day04/day04.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# --- Day 4: The Ideal Stocking Stuffer ---
+# https://adventofcode.com/2015/day/4
+#
+# https://github.com/AJPfleger
+
+echo "--- Day 4: The Ideal Stocking Stuffer ---"
+
+# Part 1
+
+i=0
+while : ; do
+  echo "${i}"
+
+  # "MD5(stdin)= 000001dbbfa3a5c83a2d506429c7b00e"
+  hash=$(echo -n "bgvyzdsv${i}" | openssl md5)
+
+  if [ "${hash:12:5}" = "00000" ]
+  then
+    break
+  fi
+
+  ((i+=1))
+done
+
+echo "Part1: Lowest number is ${i}"
+
+
+# Part 2 (let's see if it is fast enough)
+
+while : ; do
+  echo "${i}"
+
+  # "MD5(stdin)= 000001dbbfa3a5c83a2d506429c7b00e"
+  hash=$(echo -n "bgvyzdsv${i}" | openssl md5)
+
+  if [ "${hash:12:6}" = "000000" ]
+  then
+    break
+  fi
+
+  ((i+=1))
+done
+
+echo "Part2: Lowest number is ${i}"
