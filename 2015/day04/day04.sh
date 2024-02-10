4e459f6f05334367899ef0a644287af4af9814a6
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-02-10 08:59:27 +0100

    add: 2015/day04p2

--- 2015/day04/day04.sh
+++ 2015/day04/day04.sh
@@ -7,6 +7,8 @@
 
 echo "--- Day 4: The Ideal Stocking Stuffer ---"
 
+# This method is quite slow and needs a few hours.
+
 # Part 1
 
 i=0
@@ -27,12 +29,11 @@ done
 echo "Part1: Lowest number is ${i}"
 
 
-# Part 2 (let's see if it is fast enough)
+# Part 2
 
 while : ; do
   echo "${i}"
 
-  # "MD5(stdin)= 000001dbbfa3a5c83a2d506429c7b00e"
   hash=$(echo -n "bgvyzdsv${i}" | openssl md5)
 
   if [ "${hash:12:6}" = "000000" ]
