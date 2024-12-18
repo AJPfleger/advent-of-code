b042585b611efd47dc3aee472ac29225027ac783
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-12-18 19:09:30 +0100

    2024/17p2

--- 2024/day17/day17.py
+++ 2024/day17/day17.py
@@ -9,6 +9,7 @@ https://github.com/AJPfleger
 """
 
 import re
+import random
 
 from pathlib import Path
 
@@ -49,8 +50,7 @@ def do_instruction(register, program, position, out):
 
     match instruction:
         case 0:  # adv
-            register[0] //= 2**combo  # maybe math.trunc()
-            # print(register)
+            register[0] //= 2**combo
         case 1:  # bxl
             register[1] ^= combo_operand
         case 2:  # bst
@@ -70,23 +70,79 @@ def do_instruction(register, program, position, out):
     return position
 
 
+def run_program(register, program):
+    position = 0
+    max_position = len(program)
+
+    out = []
+    for _ in range(100000):
+        position = do_instruction(register, program, position, out)
+        if position >= max_position:
+            break
+    return out
+
+
+def format_output(out):
+    out_string = ""
+    for x in out:
+        out_string += str(x)
+        out_string += ","
+    return out_string[:-1]
+
+
+# A reversed engineered version of the real input. We have some branching which
+# we use by a classy random choice.
+def random_walk():
+    A = 0
+    for p in reversed(program):
+        # print(p)
+        p6 = p ^ 6
+        R_possible = []
+        for R in range(8):
+            A_try = A * 8 + R
+            B = R ^ 1
+
+            if p6 == B ^ ((A_try // (2**B)) % 8):
+                R_possible.append(R)
+        if R_possible == []:
+            return
+        R_rand = random.choice(R_possible)
+        A = A * 8 + R_rand
+    return A
+
+
 print("\n--- Day 17: Chronospatial Computer ---")
 
 filename = "input.txt"
 
+print("\n--- Part 1 ---")
+
 register, program = parse_input(filename)
 
-print(register)
-print(program)
-position = 0
-max_position = len(program)
+out = run_program(register, program)
 
-print("\n--- Part 1 ---")
-out = []
-for _ in range(100):
-    # print(position)
-    position = do_instruction(register, program, position, out)
-    if position >= max_position:
-        break
+print(format_output(out))
 
-print(f"out: {out}")
+print("\n--- Part 2---")
+# Important to notice is, that all our programs end with (3, 0). And have no
+# other (3, x) operations inside. This means, that we repeat the programs,
+# until we reach the end with A = 0. This makes it possible, to unroll the
+# programs from the end and do reverted operations. Since we are looking for
+# the smallest integer A we have enough constraints to do it (hopefully).
+# Worst case, we branch 8 times in each step.
+
+A_rand = set()
+for _ in range(10000):
+    val = random_walk()
+    if val:
+        A_rand.add(val)
+
+A = min(A_rand)
+
+register, program = parse_input(filename)
+register[0] = A
+out = run_program(register, program)
+if out == program:
+    print(A)
+else:
+    print("couldn't find a valid value for A")
